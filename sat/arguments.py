0c5052d265626f563e7222dac24ceb9a1657e3ac
YTEP-ZHI <ytepjz@gmail.com>
2024-10-28 12:08:56 +0000

    some improvements

--- sat/arguments.py
+++ sat/arguments.py
@@ -72,6 +72,7 @@ def add_evaluation_args(parser):
     group.add_argument('--sampling_video_size', type=list, default=[480, 720])
     group.add_argument('--n_prediction_round', type=int, default=1)
     group.add_argument('--use_ema', type=bool, default=False)
+    group.add_argument('--apply_traj', type=bool, default=False)
 
     return parser
 
